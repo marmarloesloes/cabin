aa3b5facc2564048025beb5ac944c4e5827357bb
Marloes de Haan <marmarloesloes@gmail.com>
2024-11-21 13:23:02 +0100

    swipe functionality

--- script.js
+++ script.js
@@ -22,37 +22,62 @@ function myFunction(x) {
   }
 
 
-  //carousel
   const slides = document.querySelectorAll('.slide');
-let index = 0;
-
-function prevSlide(){
-    slides[index].classList.remove('active');
-    index--;
-
-    if(index < 0)
-        index = slides.length -1;
-
-    slides[index].classList.add('active');      
-}
-
-document.querySelector('.prev').addEventListener('click', e => {
-    prevSlide();
-});
-
-function nextSlide(){
-    slides[index].classList.remove('active');
-    index++;
-
-    if(index > slides.length -1)
-        index = 0;
-
-    slides[index].classList.add('active');      
-}
-
-document.querySelector('.next').addEventListener('click', e => {
-    nextSlide();
-});
-
-
-
+  let index = 0;
+  
+  // Function to change the slide
+  function prevSlide(){
+      slides[index].classList.remove('active');
+      index--;
+  
+      if(index < 0)
+          index = slides.length - 1;
+  
+      slides[index].classList.add('active');
+  }
+  
+  document.querySelector('.prev').addEventListener('click', e => {
+      prevSlide();
+  });
+  
+  function nextSlide(){
+      slides[index].classList.remove('active');
+      index++;
+  
+      if(index > slides.length - 1)
+          index = 0;
+  
+      slides[index].classList.add('active');
+  }
+  
+  document.querySelector('.next').addEventListener('click', e => {
+      nextSlide();
+  });
+  
+  // Swipe functionality
+  
+  let touchstartX = 0;
+  let touchendX = 0;
+  
+  const slider = document.getElementById('slider'); // The slider container
+  
+  slider.addEventListener('touchstart', (e) => {
+      touchstartX = e.changedTouches[0].screenX;
+  });
+  
+  slider.addEventListener('touchend', (e) => {
+      touchendX = e.changedTouches[0].screenX;
+      handleSwipe();
+  });
+  
+  function handleSwipe() {
+      if (touchendX < touchstartX) {
+          // Swiped left
+          nextSlide();
+      }
+      if (touchendX > touchstartX) {
+          // Swiped right
+          prevSlide();
+      }
+  }
+  
