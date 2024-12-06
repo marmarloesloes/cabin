77ef9816ac968946cae2c39d6b1b457a1f4a4924
Marloes de Haan <marmarloesloes@gmail.com>
2024-12-06 13:22:42 +0100

    carousel adapts to content

--- vragen.js
+++ vragen.js
@@ -1,17 +1,19 @@
 var acc = document.getElementsByClassName("accordion-header");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("actief");
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("actief");
 
-    var panel = this.nextElementSibling;
+        var panel = this.nextElementSibling;
 
-    if (panel.classList.contains("open")) {
-        panel.classList.remove("open");
-      } else {
-        /* Otherwise, open the panel by adding the "open" class */
-        panel.classList.add("open");
-      }
+        if (panel.classList.contains("open")) {
+            // Collapse the accordion
+            panel.classList.remove("open");
+            panel.style.maxHeight = null; // Reset maxHeight to collapse
+        } else {
+            // Expand the accordion
+            panel.classList.add("open");
+            panel.style.maxHeight = panel.scrollHeight + "px"; // Dynamically set maxHeight
+        }
     });
 }
