188a807612411380034902f58306179be11fb976
Marloes de Haan <marmarloesloes@gmail.com>
2025-01-06 14:15:01 +0100

    favicon added and some spacing fixes

--- vragen.js
+++ vragen.js
@@ -2,18 +2,17 @@ var acc = document.getElementsByClassName("accordion-header");
 
 for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
-        this.classList.toggle("actief");
-
         var panel = this.nextElementSibling;
 
+        // Toggle the accordion
         if (panel.classList.contains("open")) {
             // Collapse the accordion
             panel.classList.remove("open");
-            panel.style.maxHeight = null; // Reset maxHeight to collapse
+            panel.style.maxHeight = "0"; // Collapse with max-height: 0
         } else {
             // Expand the accordion
             panel.classList.add("open");
-            panel.style.maxHeight = panel.scrollHeight + "px"; // Dynamically set maxHeight
+            panel.style.maxHeight = panel.scrollHeight + "px"; // Dynamically set max-height
         }
     });
 }
